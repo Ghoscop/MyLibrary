5bf452d080fe9cf928ad87ac847b64b017f091ee
SOS-Rota <56131014+Ghoscop@users.noreply.github.com>
2026-05-23 09:39:13 -0300

    feat(frontend): implementar CRUD visual de categorias

--- mylibrary-frontend/src/app/app.routes.ts
+++ mylibrary-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Categorias } from './pages/categorias/categorias';
+import { Livros } from './pages/livros/livros';
+import { Emprestimos } from './pages/emprestimos/emprestimos';
+
+export const routes: Routes = [
+  { path: '', component: Dashboard},
+  { path: 'categorias', component: Categorias },
+  { path: 'livros', component: Livros },
+  { path: 'emprestimos', component: Emprestimos }
+];
--- mylibrary-frontend/src/app/pages/categorias/categorias.ts
+++ mylibrary-frontend/src/app/pages/categorias/categorias.ts
@@ -1,9 +1,66 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule} from '@angular/common';
+
+import { Categoria } from '../../models/categoria';
+import { CategoriaService } from '../../services/categoria';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-categorias',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './categorias.html',
   styleUrl: './categorias.css',
 })
-export class Categorias {}
+export class Categorias implements OnInit{
+
+  private service: CategoriaService = inject(CategoriaService);
+
+  categorias: Categoria[] = [];
+
+  novaCategoria: Categoria = {
+    nome: '',
+    descricao: ''
+  };
+
+  ngOnInit(): void {
+    this.carregarCategorias();
+  }
+
+  carregarCategorias(): void {
+    this.service.findAll().subscribe({
+      next: (dados) => {
+        this.categorias = dados;
+      }
+    });
+  }
+
+
+  criarCategoria(): void {
+
+    this.service.criar(this.novaCategoria).subscribe({
+      next: () => {
+
+        this.novaCategoria = {
+          nome: '',
+          descricao: ''
+        };
+
+        this.carregarCategorias();
+      }
+    });
+  }
+
+  excluirCategoria(id: number): void {
+    this.service.excluir(id).subscribe({
+      next: () => {
+        this.carregarCategorias();
+      }
+    });
+  }
+
+}
