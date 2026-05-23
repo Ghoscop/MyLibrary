f6058186ebd1643087fe5a79be8886cf566d88f8
SOS-Rota <56131014+Ghoscop@users.noreply.github.com>
2026-05-23 09:15:06 -0300

    feat(frontend): criar estrutura de paginas e services

---
+++ mylibrary-frontend/src/app/pages/emprestimos/emprestimos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emprestimos',
+  imports: [],
+  templateUrl: './emprestimos.html',
+  styleUrl: './emprestimos.css',
+})
+export class Emprestimos {}
