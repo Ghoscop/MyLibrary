0a4744a2c9df9e19bc7882056c830244d10c680c
SOS-Rota <56131014+Ghoscop@users.noreply.github.com>
2026-05-24 11:27:37 -0300

    fix(backend): corrigir teste de contexto Spring Boot

--- mylibrary-backend/src/test/java/mylibrary_backend/library/LibraryApplicationTests.java
+++ mylibrary-backend/src/test/java/mylibrary_backend/library/LibraryApplicationTests.java
@@ -1,13 +1,13 @@
 package mylibrary_backend.library;
 
+import com.example.mylibrary.LibraryApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LibraryApplication.class)
 class LibraryApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
